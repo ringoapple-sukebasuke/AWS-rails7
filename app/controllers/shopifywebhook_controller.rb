2f3f907664b2fd3ec7d5a6fe4f2a8f14814291df
Okada <ringoapple.sukebasuke@gmail.com>
2023-10-19 16:30:35 +0900

    slackのwebhookを更新

--- app/controllers/shopifywebhook_controller.rb
+++ app/controllers/shopifywebhook_controller.rb
@@ -19,7 +19,7 @@ class ShopifywebhookController < ApplicationController
   end
 
   def send_to_slack(message)
-    slack_webhook_url = 'https://hooks.slack.com/services/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'
+    slack_webhook_url = 'https://hooks.slack.com/services/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'
 
     conn = Faraday.new do |faraday|
       faraday.request :json
